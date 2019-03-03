ffbf428e03a49001e72a9d58a1f94119a22dc03c
skaffin17 <47083338+skaffin17@users.noreply.github.com>
2019-03-03 17:10:54 +0100

    Add files via upload

---
+++ gh8.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+#encoding: utf-8
+
+import sys
+import RPi.GPIO as GPIO
+import thingspeak
+import datetime
+import Adafruit_DHT
+from time import sleep
+import urllib
+from RPLCD import CharLCD
+
+chId = 673945                                   # Thingspeak Channel Id
+tsKey='NAK14Z0XFYQHBRGA'                        # Thingspeak API key
+tsUrl='https://api.thingspeak.com/update?'      # Thingspeak data update url
+ts = thingspeak.Channel(tsUrl ,tsKey, chId)
+
+# Read temperature and humidity
+def readDHT():
+    humi, temp = Adafruit_DHT.read_retry(11, 18)
+    if humi is not None and temp is not None:
+        print('Temp: {0:0.1f}*C  Humidity: {1:0.1f}%'.format(temp, humi))
+        return (str(temp), str(humi))
+    else:
+        print('Did not get a reading')
+
+# Diplay temperature and humidity on screen
+def display():
+    lcd = CharLCD(cols=16, rows=2, pin_rs=5, pin_e=6, pins_data=[13, 19, 26, 16])
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string("Temp: %d C" % temp)
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string("Humidity: %d %%" % humi)
+    
+
+# Main function
+def main():
+
+    global tsKey
+    global tsUrl
+
+    while True:
+      try:
+        readDHT()
+        display()
+        data = {
+            "field1":temp,
+            "field2":humi
+            }
+        ts.update(data)
+        print ("Data sent")
+        sleep(60)
+
+      except:
+        print("Error sending data")
+
+if __name__ == '__main__':
+    main()
+    
